2aa8bd9d2610df1ab58f281b6d07df3415714572
Maximiliano Quevedo <maxquevedo@gmail.com>
2018-06-26 17:28:24 -0400

    vamoh que se puede

--- Views/form-particular.php
+++ Views/form-particular.php
@@ -1,8 +1,21 @@
-<form class="justify-content-center container" id="form-particular" name="form-particular" method="post" action="">
+<form class="justify-content-center container jumbotron" id="form-particular" name="form-particular" method="post" action="">
 	<div class="form-group row">
 		<h1 class="text-center col">Particular</h1>
 	</div>
-	<div class="form-group">
-		<h6>Rut</h6>
+	<div class="row">
+		<div class="form-group col-lg-2 offset-lg-2">
+			<h6>Rut</h6>
+		</div>
+		<div class="form-group col-lg-4">
+			<input type="text" class="form-control" name="rut" id="rut">
+		</div>
+	</div>
+	<div class="row">
+		<div class="col">
+			
+		</div>
+		<div class="col">
+			
+		</div>
 	</div>
 </form>
